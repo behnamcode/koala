34bd7d6a4b3c5c4e424a9ed7b456b552ccb090c4
behnamcode <behnamfeizi@gmail.com>
2020-02-09 13:10:14 +0330

    background color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+// next line is fo creating global style
+import { createGlobalStyle } from 'styled-components';
 
+//defining a global style 0-2
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: ${props => props.bgColor ? '#007090' : '#77AD78'};
+    color: ${props => props.whiteColor ? 'white' :'black' };
+    
+    }
+  }
+`;
 function App() {
   return (
-    <div>Hello SliceLine</div>
+    //0-3
+    <>
+      <GlobalStyle whiteColor  bgColor/>
+      <div>Hello SliceLine</div>
+    </>
   );
 }
 
