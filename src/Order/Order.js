c375c1599a57248930920838b8e4826cfa08c2de
behnamcode <behnamfeizi@gmail.com>
2020-02-11 08:36:25 +0330

    List orders

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -24,13 +24,32 @@ const OrderContent = styled(DialogContent)`
   padding: 20px;
   height: 100%;
 `;
+// step 26
+const OrderContainer = styled.div `
+    padding: 10px 0px;
+    border-bottom : 1px solid grey;
+`
+const OrderItem = styled.div `
+    padding:10px 0px;
+`
 export function Order({ orders }) {
   return (
     <OrderStyled>
       {orders.length === 0 ? (
         <OrderContent>Your order's looking pretty empty.</OrderContent>
       ) : (
-        <OrderContent>Found {orders.length} orders</OrderContent>
+        <OrderContent>
+          <OrderContainer>
+              Your order:
+          </OrderContainer>
+          {orders.map(order =>(
+            <OrderContainer>
+              <OrderItem>
+                {order.name}
+              </OrderItem>
+            </OrderContainer>
+          ))}
+        </OrderContent>
       )}
       <DialogFooter>
         <ConfirmButton>Checkout</ConfirmButton>
