83b44517f6d1dce28e2c9694ac95520ddf6dad9c
behnamcode <behnamfeizi@gmail.com>
2020-02-12 11:49:53 +0330

    Add total,tax,subtotal

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -6,7 +6,7 @@ import {
   ConfirmButton
 } from '../FoodDialog/FoodDialog';
 import { formatPrice } from '../Data/FoodData';
-import {getPrice} from '../FoodDialog/FoodDialog'
+import { getPrice } from '../FoodDialog/FoodDialog';
 //23
 
 const OrderStyled = styled.div`
@@ -27,36 +27,61 @@ const OrderContent = styled(DialogContent)`
   height: 100%;
 `;
 // step 26
-const OrderContainer = styled.div `
-    padding: 10px 0px;
-    border-bottom : 1px solid grey;
-`
-const OrderItem = styled.div `
-    padding:10px 0px;
-    display:grid;
-    grid-template-columns:20px 150px 20px 60px;
-    justify-content:space-between;
-`
+const OrderContainer = styled.div`
+  padding: 10px 0px;
+  border-bottom: 1px solid grey;
+`;
+const OrderItem = styled.div`
+  padding: 10px 0px;
+  display: grid;
+  grid-template-columns: 20px 150px 20px 60px;
+  justify-content: space-between;
+`;
 export function Order({ orders }) {
+  //step 30
+  const subtotal = orders.reduce((total, order) => {
+    return total + getPrice(order);
+  }, 0);
+  const tax = subtotal * 0.07;
+  const total = subtotal+tax;
   return (
     <OrderStyled>
       {orders.length === 0 ? (
         <OrderContent>Your order's looking pretty empty.</OrderContent>
       ) : (
         <OrderContent>
-          <OrderContainer>
-              Your order:
-          </OrderContainer>
-          {orders.map(order =>(
+          <OrderContainer>Your order:</OrderContainer>
+          {orders.map(order => (
             <OrderContainer>
               <OrderItem>
-              <div>{order.quantity}</div>
+                <div>{order.quantity}</div>
                 <div>{order.name}</div>
-                <div/>
+                <div />
                 <div>{formatPrice(getPrice(order))}</div>
               </OrderItem>
             </OrderContainer>
           ))}
+          <OrderContainer>
+            <OrderItem>
+              <div />
+              <div>Sub-Total</div>
+              <div>{formatPrice(subtotal)}</div>
+            </OrderItem>
+          </OrderContainer>
+          <OrderContainer>
+            <OrderItem>
+              <div />
+              <div>tax</div>
+              <div>{formatPrice(tax)}</div>
+            </OrderItem>
+          </OrderContainer>
+          <OrderContainer>
+            <OrderItem>
+              <div />
+              <div>Total</div>
+              <div>{formatPrice(total)}</div>
+            </OrderItem>
+          </OrderContainer>
         </OrderContent>
       )}
       <DialogFooter>
